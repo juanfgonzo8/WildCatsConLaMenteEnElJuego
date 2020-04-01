ea982353f3e4cbc612b7526b4ede1ecb889f674f
Juan Felipe Gonzalez <jf.gonzalezc@uniandes.edu.co>
2020-04-01 11:06:57 -0500

    Se crea codigo para crear subset

---
+++ CrearSubsetEquilibrado.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+
+#Path JF: /Users/johngonzalez/Desktop/Trabajos JF/Documents/U/Octavo Semestre/Vision Artificial/Proyecto/iwildcam-2019-fgvc6/train.csv
+csv_train = pd.read_csv('/Users/johngonzalez/Desktop/Trabajos JF/Documents/U/Octavo Semestre/Vision Artificial/Proyecto/iwildcam-2019-fgvc6/train.csv')
+anots = csv_train['category_id'].to_numpy()
+arch = csv_train['file_name'].tolist()
+unics, conts = np.unique(anots, return_counts=True)
+num_min = np.min(conts[:-1])
+subset = []
+for i in unics:
+    pos = np.where(anots==i)[0]
+    #print(pos)
+    selec = pos[0:num_min]
+    noms = [arch[j] for j in selec]
+    subset.append(noms)
