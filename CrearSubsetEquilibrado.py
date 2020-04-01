f89ee079bdb816506a2deee841f5e7afb617667a
Juan Felipe Gonzalez <jf.gonzalezc@uniandes.edu.co>
2020-04-01 13:22:17 -0500

    Se crea una lista sola con los paths completos

--- CrearSubsetEquilibrado.py
+++ CrearSubsetEquilibrado.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import numpy as np
 
-#Path JF: /Users/johngonzalez/Desktop/Trabajos JF/Documents/U/Octavo Semestre/Vision Artificial/Proyecto/iwildcam-2019-fgvc6/train.csv
+#Path JF anots: /Users/johngonzalez/Desktop/Trabajos JF/Documents/U/Octavo Semestre/Vision Artificial/Proyecto/iwildcam-2019-fgvc6/train.csv
+#Path JF train: /Users/johngonzalez/Desktop/Trabajos JF/Documents/U/Octavo Semestre/Vision Artificial/Proyecto/iwildcam-2019-fgvc6/train_images/
 csv_train = pd.read_csv('/Users/johngonzalez/Desktop/Trabajos JF/Documents/U/Octavo Semestre/Vision Artificial/Proyecto/iwildcam-2019-fgvc6/train.csv')
+path_train = '/Users/johngonzalez/Desktop/Trabajos JF/Documents/U/Octavo Semestre/Vision Artificial/Proyecto/iwildcam-2019-fgvc6/train_images/'
 anots = csv_train['category_id'].to_numpy()
 arch = csv_train['file_name'].tolist()
 unics, conts = np.unique(anots, return_counts=True)
@@ -14,3 +16,7 @@ for i in unics:
     selec = pos[0:num_min]
     noms = [arch[j] for j in selec]
     subset.append(noms)
+lista = []
+for i in subset:
+    for j in i:
+        lista.append(path_train+j)
