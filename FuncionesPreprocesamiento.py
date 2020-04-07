d6fd78b4996313debb93f8699ee5d0d6a63c0e24
Juan Felipe Gonzalez <jf.gonzalezc@uniandes.edu.co>
2020-04-07 11:01:46 -0500

    Se crean funciones para CLAHE y diferentes White Balancing

---
+++ FuncionesPreprocesamiento.py
@@ -0,0 +1,35 @@
+import numpy as np
+import cv2
+
+# Funcion para CLAHE
+# Recibe BGR y saca BGR
+def miCLAHE(im):
+    elClahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(16,16)) #puede probarse (8,8)
+    labs = cv2.cvtColor(im, cv2.COLOR_BGR2Lab)
+    labs[0] = elClahe.apply(labs[0])
+    clahed = cv2.cvtColor(labs,cv2.COLOR_Lab2BGR)
+    return clahed
+
+#Funcion para Simple white balance
+# Recibe BGR y saca BGR
+def miWBsimple(im):
+    elWB = cv2.xphoto.createSimpleWB()
+    elWB.setP(0.4) #Se puede cambiar
+    WBed = elWB.balanceWhite(im)
+    return WBed
+
+# Funcion para Gray-world white balance
+# Recibe BGR y saca BGR
+def miWBgrayworld(im):
+    elWB = cv2.xphoto.createGrayworldWB()
+    elWB.setSaturationThreshold(0.9) #Se puede cambiar
+    WBed = elWB.balanceWhite(im)
+    return WBed
+
+# Funcion para learning-based automatic white balance
+# Recibe BGR y saca BGR
+def miWB_LB(im):
+    elWB = cv2.xphoto.createLearningBasedWB()
+    elWB.setSaturationThreshold(0.99) #Se puede cambiar
+    WBed = elWB.balanceWhite(im)
+    return WBed
