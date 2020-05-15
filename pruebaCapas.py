3c750d38b736db7bc9e0e0bf14e0383dc77406de
Juan Felipe Gonzalez <jf.gonzalezc@uniandes.edu.co>
2020-05-15 16:28:38 -0500

    Se mira el numero de capas 2

--- pruebaCapas.py
+++ pruebaCapas.py
@@ -84,6 +84,30 @@ model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='categorical_crossent
 
 for i, layer in enumerate(base_model.layers):
    print(i, layer.name)
-print('Ahora model')
+print('\n\nAhora model')
 for i, layer in enumerate(model.layers):
+   print(i, layer.name)
+
+##
+# create the base pre-trained model
+base_model = ResNet152(weights='imagenet', include_top=True)
+
+# add a global spatial average pooling layer
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+# let's add a fully-connected layer
+x = Dense(1024, activation='relu')(x)
+# and a logistic layer -- let's say we have 200 classes
+predictions = Dense(14, activation='softmax')(x)
+
+# this is the model we will train
+model = Model(inputs=base_model.input, outputs=predictions)
+
+# first: train only the top layers (which were randomly initialized)
+# i.e. freeze all convolutional InceptionV3 layers
+for layer in base_model.layers:
+    layer.trainable = False
+
+print('\n\nAhora con include top')
+for i, layer in enumerate(base_model.layers):
    print(i, layer.name)
