c8790118e6fa36b6314061123acd4a6e82aaf28a
Juan Felipe Gonzalez <jf.gonzalezc@uniandes.edu.co>
2020-05-20 02:50:34 -0500

    +

--- Archivos Viejos/main_inception3completo.py
+++ Archivos Viejos/main_inception3completo.py
@@ -18,7 +18,7 @@ from keras import backend as K
 # from tensorflow.python.client import device_lib
 # print(device_lib.list_local_devices())
 
-sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(log_device_placement=True))
 
 K.tensorflow_backend._get_available_gpus()
 print('Llego aca')
