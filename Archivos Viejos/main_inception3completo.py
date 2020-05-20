a0ea703278501c9f583893c05cec8b3ebe1c06ed
Juan Felipe Gonzalez <jf.gonzalezc@uniandes.edu.co>
2020-05-19 20:13:47 -0500

    Keras returns 5

--- Archivos Viejos/main_inception3completo.py
+++ Archivos Viejos/main_inception3completo.py
@@ -15,6 +15,7 @@ os.environ['CUDA_VISIBLE_DEVICES'] = "1"
 
 from keras import backend as K
 sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(log_device_placement=True))
+K.set_session(sess)
 
 ##
 #Se establecen los paths
