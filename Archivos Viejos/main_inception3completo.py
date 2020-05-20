58be898d80aee305404c3b448dc30b0cdbc07aad
Juan Felipe Gonzalez <jf.gonzalezc@uniandes.edu.co>
2020-05-20 02:49:36 -0500

    Los 50 de Joselito

--- Archivos Viejos/main_inception3completo.py
+++ Archivos Viejos/main_inception3completo.py
@@ -15,8 +15,10 @@ os.environ['CUDA_VISIBLE_DEVICES'] = "1"
 
 from keras import backend as K
 
-from tensorflow.python.client import device_lib
-print(device_lib.list_local_devices())
+# from tensorflow.python.client import device_lib
+# print(device_lib.list_local_devices())
+
+sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
 
 K.tensorflow_backend._get_available_gpus()
 print('Llego aca')
