9366b16376c46fe0e2889300a75884dfde68da03
Juan Felipe Gonzalez <jf.gonzalezc@uniandes.edu.co>
2020-05-19 21:48:28 -0500

    Keras returns 20

--- Archivos Viejos/main_inception3completo.py
+++ Archivos Viejos/main_inception3completo.py
@@ -27,39 +27,40 @@ path_train = '/media/user_home2/vision2020_01/Data/iWildCam2019/train_images'
 
 ##
 #Se crea el modelo
-# create the base pre-trained model
-base_model = InceptionV3(weights='imagenet', include_top=False)
+with tf.device('/gpu:0'):
+    # create the base pre-trained model
+    base_model = InceptionV3(weights='imagenet', include_top=False)
 
-# add a global spatial average pooling layer
-x = base_model.output
-x = GlobalAveragePooling2D()(x)
-# let's add a fully-connected layer
-x = Dense(1024, activation='relu')(x)
-# and a logistic layer -- let's say we have 200 classes
-predictions = Dense(14, activation='softmax')(x)
+    # add a global spatial average pooling layer
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    # let's add a fully-connected layer
+    x = Dense(1024, activation='relu')(x)
+    # and a logistic layer -- let's say we have 200 classes
+    predictions = Dense(14, activation='softmax')(x)
 
-# this is the model we will train
-model = Model(inputs=base_model.input, outputs=predictions)
+    # this is the model we will train
+    model = Model(inputs=base_model.input, outputs=predictions)
 
-# first: train only the top layers (which were randomly initialized)
-# i.e. freeze all convolutional InceptionV3 layers
-# for layer in base_model.layers:
-#     layer.trainable = False
+    # first: train only the top layers (which were randomly initialized)
+    # i.e. freeze all convolutional InceptionV3 layers
+    # for layer in base_model.layers:
+    #     layer.trainable = False
 
-# compile the model (should be done *after* setting layers to non-trainable)
-# model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+    # compile the model (should be done *after* setting layers to non-trainable)
+    # model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
 
-##
-#Se cargan los datos
-train_df = pd.read_csv(path_csv)
-train_df['category_id'] = train_df['category_id'].astype(str)
+    ##
+    #Se cargan los datos
+    train_df = pd.read_csv(path_csv)
+    train_df['category_id'] = train_df['category_id'].astype(str)
 
-batch_size=32
-img_size = 299
-nb_epochs = 10
+    batch_size=32
+    img_size = 299
+    nb_epochs = 10
 
-train_datagen = ImageDataGenerator(rescale=1./255, validation_split=0.25)
-train_generator = train_datagen.flow_from_dataframe(
+    train_datagen = ImageDataGenerator(rescale=1./255, validation_split=0.25)
+    train_generator = train_datagen.flow_from_dataframe(
         dataframe = train_df,
         directory = path_train,
         x_col = 'file_name', y_col = 'category_id',
@@ -68,7 +69,7 @@ train_generator = train_datagen.flow_from_dataframe(
         class_mode='categorical',
         subset='training')
 
-validation_generator  = train_datagen.flow_from_dataframe(
+    validation_generator  = train_datagen.flow_from_dataframe(
         dataframe = train_df,
         directory = path_train,
         x_col = 'file_name', y_col = 'category_id',
@@ -77,45 +78,45 @@ validation_generator  = train_datagen.flow_from_dataframe(
         class_mode='categorical',
         subset='validation')
 
-set(train_generator.class_indices)
-nb_classes = 14
+    set(train_generator.class_indices)
+    nb_classes = 14
 
-##Se entrena el modelo usando fine-tune
+    ##Se entrena el modelo usando fine-tune
 
-# train the model on the new data for a few epochs
-#model.fit(...)
+    # train the model on the new data for a few epochs
+    #model.fit(...)
 
-# Train model
-# history = model.fit_generator(
-#             train_generator,
-# #             steps_per_epoch = train_generator.samples // batch_size,
-#             steps_per_epoch = 100,
-#             validation_data = validation_generator,
-# #             validation_steps = validation_generator.samples // batch_size,
-#             validation_steps = 50,
-#             epochs = nb_epochs,
-#             verbose=2)
+    # Train model
+    # history = model.fit_generator(
+    #             train_generator,
+    # #             steps_per_epoch = train_generator.samples // batch_size,
+    #             steps_per_epoch = 100,
+    #             validation_data = validation_generator,
+    # #             validation_steps = validation_generator.samples // batch_size,
+    #             validation_steps = 50,
+    #             epochs = nb_epochs,
+    #             verbose=2)
 
-# at this point, the top layers are well trained and we can start fine-tuning
-# convolutional layers from inception V3. We will freeze the bottom N layers
-# and train the remaining top layers.
+    # at this point, the top layers are well trained and we can start fine-tuning
+    # convolutional layers from inception V3. We will freeze the bottom N layers
+    # and train the remaining top layers.
 
-# let's visualize layer names and layer indices to see how many layers
-# we should freeze:
-#for i, layer in enumerate(base_model.layers):
-#   print(i, layer.name)
+    # let's visualize layer names and layer indices to see how many layers
+    # we should freeze:
+    #for i, layer in enumerate(base_model.layers):
+    #   print(i, layer.name)
 
-# we chose to train the top 2 inception blocks, i.e. we will freeze
-# the first 249 layers and unfreeze the rest:
-# for layer in model.layers[:249]:
-#    layer.trainable = False
-# for layer in model.layers[249:]:
-#    layer.trainable = True
+    # we chose to train the top 2 inception blocks, i.e. we will freeze
+    # the first 249 layers and unfreeze the rest:
+    # for layer in model.layers[:249]:
+    #    layer.trainable = False
+    # for layer in model.layers[249:]:
+    #    layer.trainable = True
 
 
-##
-#Metrica final
-def f1(y_true, y_pred):
+    ##
+    #Metrica final
+    def f1(y_true, y_pred):
     def recall(y_true, y_pred):
         """Recall metric.
 
@@ -145,18 +146,18 @@ def f1(y_true, y_pred):
     recall = recall(y_true, y_pred)
     return 2*((precision*recall)/(precision+recall+K.epsilon()))
 
-##
-# we need to recompile the model for these modifications to take effect
-# we use SGD with a low learning rate
-from keras.optimizers import SGD
-model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy', metrics=['accuracy',f1])
+    ##
+    # we need to recompile the model for these modifications to take effect
+    # we use SGD with a low learning rate
+    from keras.optimizers import SGD
+    model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy', metrics=['accuracy',f1])
 
-# we train our model again (this time fine-tuning the top 2 inception blocks
-# alongside the top Dense layers
-#model.fit(...)
+    # we train our model again (this time fine-tuning the top 2 inception blocks
+    # alongside the top Dense layers
+    #model.fit(...)
 
-# Train model
-with tf.device('/gpu:0'):
+    # Train model
+
     history = model.fit_generator(
                 train_generator,
     #             steps_per_epoch = train_generator.samples // batch_size,
