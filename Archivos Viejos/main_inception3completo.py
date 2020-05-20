bbee2a81d6f547c799945a6faa4a1978a6d72020
Juan Felipe Gonzalez <jf.gonzalezc@uniandes.edu.co>
2020-05-20 02:07:49 -0500

    Keras returns 46

--- Archivos Viejos/main_inception3completo.py
+++ Archivos Viejos/main_inception3completo.py
@@ -170,13 +170,13 @@ with tf.device('/device:XLA_GPU:0'):
     print('Hasta aqui todo bien')
     print('Hasta aqui todo bien')
     #with tf.device('/device:XLA_GPU:0'):
-    sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(log_device_placement=True))
-    print(sess.run(model.fit_generator(
-                train_generator,
-    #             steps_per_epoch = train_generator.samples // batch_size,
-                steps_per_epoch = 100,
-                validation_data = validation_generator,
-    #             validation_steps = validation_generator.samples // batch_size,
-                validation_steps = 50,
-                epochs = nb_epochs,
-                verbose=2)))
+history = model.fit_generator(
+            train_generator,
+#             steps_per_epoch = train_generator.samples // batch_size,
+            steps_per_epoch = 100,
+            validation_data = validation_generator,
+#             validation_steps = validation_generator.samples // batch_size,
+            validation_steps = 50,
+            epochs = nb_epochs,
+            verbose=2,
+            workers=50)
