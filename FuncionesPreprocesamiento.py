2e11aa38604bb1c8b351e799c46aa06b90826a2b
Juan Felipe Gonzalez <jf.gonzalezc@uniandes.edu.co>
2020-05-26 00:03:45 -0500

    Se prueba InceptionResnet2 con prepro 13

--- FuncionesPreprocesamiento.py
+++ FuncionesPreprocesamiento.py
@@ -9,8 +9,8 @@ def miCLAHE(im):
     elClahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(16, 16))
 
     labs = cv2.cvtColor(im, cv2.COLOR_BGR2Lab)
-    print(type(labs[0,0,0]))
     labs[:,:,0] = elClahe.apply(np.uint16(labs[:,:,0]))
+    print(type(labs))
     clahed = cv2.cvtColor(labs,cv2.COLOR_Lab2BGR)
     clahed = cv2.cvtColor(clahed, cv2.COLOR_BGR2RGB)
     return clahed
