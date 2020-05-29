289b3d7551937f6f9ffb180d52c8cc248840cedf
Juan Felipe Gonzalez <jf.gonzalezc@uniandes.edu.co>
2020-05-28 20:27:04 -0500

    prueba 14

--- Entrada con Info Locacion/main_inceptionresnet2compLoca.py
+++ Entrada con Info Locacion/main_inceptionresnet2compLoca.py
@@ -97,7 +97,7 @@ train_generator = train_datagen.flow_from_dataframe(
         dataframe = train_df,
         directory = path_train,
         x_col = 'file_name', y_col = 'category_id',
-        target_size=(img_size,img_size),
+        target_size=(img_size,img_size,4),
         batch_size=batch_size,
         class_mode='categorical',
         subset='training')
@@ -106,7 +106,7 @@ validation_generator  = train_datagen.flow_from_dataframe(
         dataframe = train_df,
         directory = path_train,
         x_col = 'file_name', y_col = 'category_id',
-        target_size=(img_size,img_size),
+        target_size=(img_size,img_size,4),
         batch_size=batch_size,
         class_mode='categorical',
         subset='validation')
