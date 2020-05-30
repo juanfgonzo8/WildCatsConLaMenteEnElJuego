a0c0fc8508ba0b8d1389d10db73aab173a21e0fc
Juan Felipe Gonzalez <jf.gonzalezc@uniandes.edu.co>
2020-05-30 13:45:13 -0500

    otra cosa con pesos 1.1

--- Pesos Ajustados/main_inceptionresnet2compPesos.py
+++ Pesos Ajustados/main_inceptionresnet2compPesos.py
@@ -12,7 +12,7 @@ from keras.preprocessing.image import ImageDataGenerator
 import tensorflow as tf
 
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = "0"
+os.environ['CUDA_VISIBLE_DEVICES'] = "1"
 
 from keras import backend as K
 
